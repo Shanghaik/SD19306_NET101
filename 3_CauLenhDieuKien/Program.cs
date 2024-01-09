054fcb1a0ef0e6962553d90dd80935cde26aef39
shanghaik168@gmail.com <Shanghaik1998>
2024-01-09 11:22:36 +0700

    buoi3

---
+++ 3_CauLenhDieuKien/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _3_CauLenhDieuKien
+{
+    /*
+     * Câu lệnh điều kiện là những cú pháp, câu lệnh, cấu trúc mà 
+     * kết quả thực thi dựa theo 1 điều kiện nào đó mang giá trị 
+     * boolean (true/false) 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 10;
+            bool a = x == y;
+            Console.WriteLine(a);
+            //if(a)
+            //{
+            //    Console.WriteLine("x bằng y");
+            //}else
+            //{
+            //    Console.WriteLine("x khác y");
+            //}
+            // Khi chúng ta sử dụng 1 biến bool trong điều kiện
+            // thi không cần so sánh thêm nó với true hay false
+            // Sử dụng if/else để thực hiện việc in ra màn hình
+            // Học lực của sinh viên dựa theo điểm
+            // 0 đến dưới 5 -> Tạch, 5 đến dưới 6.5 -> TB, 6.5 -> Dưới 8
+            // -> Khá, 8 đến 10 -> Giỏi, ngoài khoảng -> Báo sai
+            Console.WriteLine("Enter your grade: ");
+            double grade = Convert.ToDouble(Console.ReadLine());
+            if(grade<0 || grade > 10)
+            {
+                Console.WriteLine("Wrong number");
+            }
+            else if(grade >=0 && grade < 5)
+            {
+                Console.WriteLine("You're failed");
+            }else if (grade < 6.5)
+            {
+                Console.WriteLine("You're fairy good");
+            }else if(grade < 8)
+            {
+                Console.WriteLine("You're quite good");
+            }else 
+            {
+                Console.WriteLine("You're excellent");
+            }
+            
+        }
+    }
+}
---
+++ 3_Toantu/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _3_Toantu
+{
+    /*
+     * Toán tử là những kí hiệu để đại diện cho một thao tác/phép toán, biểu thức
+     * trong lập trình. Toán tử là thành phần không thể thiếu đối với lập trình. 
+     * Có rất nhiều loại toán tử và cách để phân loại.
+     * Cách phân loại theo chức năng:
+     * - Toán tử số học (Thực thi các biểu thức số học như: + - * / %)
+     * - Toán tử quan hệ (>, <, >=, <=, !=, ==)
+     * - Toán tử logic (&& || ! ^)
+     * - Toán tử gán (=, +=, -=,...) 
+     * - Toán tử bit (&, |, ^, ~, >>, <<)
+     * Phân loại theo ngôi (ngôi ở đây là số lượng chủ thể cần có của toán tử)
+     * - 1 ngôi: --a, ++a, a++, a--, !a,...
+     * - 2 ngôi: + - * / %, gán, so sánh,...
+     * - 3 ngôi: điều kiện ? biểu thức nếu đk đúng : biểu thức nếu đk sai 
+     */
+    internal class Program
+    {
+        // 1. Nhập 2 số nguyên sau đó in ra kết quả của các phép +, -, *, / chính xác
+        // đến chữ số thập phân số 4 (Math.Round(số cần làm tròn, số chữ số thập phân))
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Nhập số a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhập số b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{a}+{b}={a + b}");
+            Console.WriteLine($"{a}-{b}={a - b}");
+            Console.WriteLine($"{a}*{b}={a * b}");
+            //Console.WriteLine($"{a}/{b}={Convert.ToDouble(a) / b}");
+            Console.WriteLine($"{a}/{b}={a * 1.0 / b}");
+            Console.WriteLine($"{a}/{b}={(double)a / b}");
+            double phepchia = a * 1.0 / b;
+            Console.WriteLine($"{a}/{b}={Math.Round(phepchia,4)}");
+            // Lưu ý: a là số nguyên mà 1.0 là số thực thì kết quả a*1.0 là số thực
+            // dẫn đến kết quả a*1.0/b là số thực
+            // 2. Thực hiện các phép gán với a và b vừa nhập
+            //Console.WriteLine($"Tăng a lên b đơn vị {a+=b}");
+            // 3. Thực hiện các phép ++, -- trên a hoặc b
+            // --a sẽ thực hiện giảm a đi 1 đơn vị trước khi câu lệnh chứa nó được thực thi
+            // a-- sẽ thực hiện giảm a đi 1 đơn vị sau khi câu lệnh chứa nó được thực thi
+            //Console.WriteLine(++a);
+            //Console.WriteLine(a);
+            // Nguyên lý ưu tiên toán tử. Mỗi toán tử có một chỉ số ưu tiên nhất định, khi
+            // chúng được dùng sẽ thực hiện phép tính được ưu tiên trước vd: ++a sẽ ưu tiên
+            // hơn so với a++
+            Console.WriteLine($"{a++},{++a},{--a},{a--},{a++},{--a}");
+            Console.WriteLine(a);
+        }
+    }
+}
