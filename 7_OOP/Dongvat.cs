3aaaa5c75ebd9ee8d766124e1a3b281f103511a3
shanghaik168@gmail.com <Shanghaik1998>
2024-01-25 11:26:56 +0700

    OOP

---
+++ 7_OOP/Dongvat.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_OOP
+{
+
+    internal class Dongvat
+    {
+        // Khai báo các thuộc tính cho đối tượng
+        private string ten = "Chó"; // mặc định là private rồi
+        string tuoi;
+        string gioitinh = " Đực";
+        int sochan = 3;
+        string noio = "Ao hồ"; // Ao hồ cũng là giá trị mặc định
+        // Property với get và set được dùng để gán hoặc lấy ra giá trị
+        // của thuộc tính nào đó
+        // Bôi đen các thuộc tính, sau đó ấn tổ hợp các phím ctrl . hoặc
+        // click chuột phải chọn Quick Action... 
+        // Sau đó chọn Encasulate Field and Use Property
+        public string Ten { get => ten; set => ten = value; }
+        public string Tuoi { get => tuoi; set => tuoi = value; }
+        public string Gioitinh { get => gioitinh; set => gioitinh = value; }
+        public int Sochan { get => sochan; set => sochan = value; }
+        public string Noio { get => noio; set => noio = value; }
+        // Constructor (hàm tạo) khỏi tạo các đối tượng với các giá trị thuộc tính
+        // Constructor không tham số
+        // Khởi tạo đối tượng với các giá trị mặc định (có thể được thiết lập)
+        // Tổ hợp phím: CTOR tab tab (Xê tê o rờ táp táp)
+        public Dongvat()
+        {
+            // Gán giá trị mặc định
+            sochan = 4; // giá trị mặc định = 4, ưu tiên giá trị nằm trong constructor
+            tuoi = "1"; // tuổi mặc định = 1
+        }
+
+        public Dongvat(string ten, string tuoi, string gioitinh, int sochan, string noio)
+        {
+            this.ten = ten; // this chính là bản thân class
+            this.tuoi = tuoi; // this.tuoi và tuoi khác gì nhau?
+            this.gioitinh = gioitinh; // this.tuoi là thuộc tính tuoi của class
+            this.sochan = sochan; // tuoi là giá trị được truyền vào (param)
+            this.noio = noio;
+        }
+
+        public Dongvat(string ten, string tuoi, string gioitinh)
+        {
+            this.ten = ten;
+            this.tuoi = tuoi;
+            this.gioitinh = gioitinh;
+        }
+        // Constructor có tham số - khởi tạo đối tượng với thuộc tính mang giá
+        // trị của các tham số được truyền vào
+        // Cách tạo:
+        // 1. Bôi đen các thuộc tính => Ctrl . => Generate Constructor
+        // 2. Click vào vùng trống trên màn hình => Ctrl . => Generate Constructor
+        // chọn các thuộc tính mà mình muốn cho vào => OK
+
+        // Các Method (Phương thức) thể hiện các hành vi của đối tượng
+        // Method có cấu tạo y hệt như function nhưng phải gắn liên với đối tượng
+        public void InThongTin()
+        {
+            Console.WriteLine($"Động vật {ten} {gioitinh }có số tuổi là: {Tuoi}");
+            Console.WriteLine($"Đang sinh sống tại {Noio} hiện còn: {sochan} chân");
+        }
+        // Nôi viết hoa thực chất sẽ trỏ đến noio viết thường nên bản chất chúng đều là 1
+        public void PhanLoai()
+        {
+            if (sochan == 2) Console.WriteLine("gia cầm");
+            else if (sochan == 4) Console.WriteLine("Gia súc");
+            else Console.WriteLine("Không xác định");
+        }
+
+
+    }
+}
---
+++ 7_OOP/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _7_OOP
+{
+    internal class Program
+    {
+        /*
+         * OOP là gì? Object Oriented Programing - Lập trình hướng đối tượng
+         * Là phương pháp lập trình sử dụng các đối tượng làm trung tâm
+         * Mỗi đối tượng sẽ có các thần phần cụ thể là:
+         * - Thuộc tính: Các giá trị thể hiện các tính chất của đối tượng
+         * - Constructor: Hàm tạo (sử dụng để khởi tạo đối tượng với các thuộc tính)
+         * - Method (Phương thức) thể hiện các hành vi của đối tượng
+         * Class (lớp) là khuôn mẫu cho các đối tượng, đối tượng (object) được
+         * gọi là các instance (thể hiện) của các class
+         * Một Class thường được tạo trong 1 file vật lý. Tuy nhiên cần phân biệt 
+         * file .cs và các class. Một file cs có thể chứa nhiều class
+         * Tất cả những thứ gì chứa thông tin, có thể được lưu thông tin lại và
+         * tương tác được => đối tượng
+         * OOP có 4 tính chất cơ bản (Đóng gói, kế thừa, tính đa hình, trừu tượng)
+         * Đóng gọi: Thể hiện khả năng truy cập
+         * Kế thừa: Thể hiện khả năng sử dụng lại các thành phần có sẵn
+         * Đa hình: thể hiện Sự đang dạng trong việc thể hiện các hành động
+         * Trừu tượng: Trừu tượng hóa các lớp đối tượng thông qua các thành phần chung
+         */
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            // Tạo 1 đối tượng
+            Dongvat dv; // Khai báo - đối tượng sẽ mang giá trị null
+            // dv.InThongTin(); // Lỗi vì đối tượng chưa được khởi tạo
+            Dongvat dv2 = new Dongvat(); // Khỏi tạo bằng constructor không tham số
+            dv2.InThongTin();
+        }
+    }
+}
